54c85fe0c63bc8c7392b03ead7f37d3f6310d0e3
PolinaQAe <138627297+PolinaQAe@users.noreply.github.com>
2024-03-03 21:55:09 +0300

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,13 @@
+public class Numbers {
+
+    public static void main(String[] args) {
+            int number = 4;
+            boolean result = number % 2 == 0;
+            if (result) {
+                System.out.println("четное");
+            } else {
+                System.out.println("Нечетное");
+            }
+        }
+    }
+
