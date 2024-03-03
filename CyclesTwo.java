8be40db64ba30133287287f92f4e36fc057a2634
PolinaQAe <138627297+PolinaQAe@users.noreply.github.com>
2024-03-03 21:54:14 +0300

    Add files via upload

---
+++ CyclesTwo.java
@@ -0,0 +1,19 @@
+public class CyclesTwo {
+public static void main (String[] args){
+
+
+
+int i = 1;
+int number = 9;
+int sum = 0;
+
+        while (i <= number) {
+sum += i;
+i++;
+        }
+        {
+                System.out.println("Сумма чисел от 1 до " + number + " равна " + sum);
+        System.out.println();
+        }
+        }
+}
