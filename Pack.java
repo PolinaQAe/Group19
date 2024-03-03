4a29536ada3b72964efad9ed88a61816f1690819
PolinaQAe <138627297+PolinaQAe@users.noreply.github.com>
2024-03-03 21:18:58 +0300

    Add files via upload

---
+++ Pack.java
@@ -0,0 +1,28 @@
+package Pack;
+
+public class Pack {
+    public static void main(String[] args) {
+        int color = 2;
+
+        if (color == 1) {
+            System.out.println("Красный");
+        }
+        if (color == 2) {
+            System.out.println("Оранжевый");
+        }
+        if (color == 3) {
+            System.out.println("Желтый");
+        }
+        if (color == 4) {
+            System.out.println("Зеленый");
+        }
+        if (color == 5) {
+            System.out.println("Голубой");
+        }
+        if (color == 6) {
+            System.out.println("Синий");
+        } else {
+            System.out.println("Фиолетовый");
+        }
+    }
+}
