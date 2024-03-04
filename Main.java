38feca2d240147d8deb03a50b3ffd4e0675ec193
PolinaQAe <138627297+PolinaQAe@users.noreply.github.com>
2024-03-04 21:05:52 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 
 public class Main {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 4;
-        int e = 5;
-        int f = 6;
-        int j = 7;
-        int k = 8;
-        int l = 9;
-        int m = 10;
-        int n = 11;
-        int o = 12;
-
-        if (a == 1&& b==2&& o==12) {
-            System.out.println("Winter");
-        } else if (c==3&& d==4&& e==5)
-        {
-            System.out.println("Spring");
-        }
-        else if (f==6&& j==7&& k==8)
-
-        {
-            System.out.println("Summer");
-        }
-        else
-        {
-            System.out.println("Autmn");
+        int num1 = 1;
+        int num2 = 3;
+        System.out.println();
+        } ;
         }
     }
-}
+
