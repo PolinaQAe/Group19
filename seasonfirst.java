273568a11c095abc95577d1dda0267818fc07517
PolinaQAe <138627297+PolinaQAe@users.noreply.github.com>
2024-03-10 21:41:06 +0300

    Update seasonfirst.java

--- seasonfirst.java
+++ seasonfirst.java
@@ -1,25 +1,27 @@
-public class seasonfirst {
+import java.util.Scanner;
 
-        public static void main (String[] args) {
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число месяца: ");
+        int day = scanner.nextInt();
 
-            int day = 5;
-            switch (day) {
-                case 1,2,12:
-                    System.out.println("Winter");
-                    break;
-                case 3,4,5:
-                    System.out.println("Spring");
-                    break;
-                case 6,7,8:
-                    System.out.println("Summer");
-                    break;
-                case 9,10,11:
-                    System.out.println("Autumn");
-                    break;
 
-            }
+        switch (day) {
+            case 1,2,12:
+                System.out.println("Winter");
+                break;
+            case 3,4,5:
+                System.out.println("Spring");
+                break;
+            case 6,7,8:
+                System.out.println("Summer");
+                break;
+            case 9,10,11:
+                System.out.println("Autumn");
+                break;
 
         }
+
     }
-
-
+}
