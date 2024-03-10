6ba13b1e5b417d0e58721e780057c144a0a1f15f
PolinaQAe <138627297+PolinaQAe@users.noreply.github.com>
2024-03-10 22:11:48 +0300

    Update CyclesTwo.java

--- CyclesTwo.java
+++ CyclesTwo.java
@@ -7,13 +7,13 @@ int i = 1;
 int number = 9;
 int sum = 0;
 
-        while (i <= number) {
+while (i <= number) {
 sum += i;
 i++;
-        }
-        {
-                System.out.println("Сумма чисел от 1 до " + number + " равна " + sum);
-        System.out.println();
-        }
-        }
+ }
+{
+ System.out.println("Сумма чисел от 1 до " + number + " равна " + sum);
+System.out.println();
+}
+}
 }
