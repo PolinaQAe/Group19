2e940b08f23f99447d4e74ad24cfa4eaa33ba549
PolinaQAe <138627297+PolinaQAe@users.noreply.github.com>
2024-03-10 22:10:29 +0300

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -1,13 +1,18 @@
-public class Numbers {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-            int number = 4;
-            boolean result = number % 2 == 0;
-            if (result) {
-                System.out.println("четное");
-            } else {
-                System.out.println("Нечетное");
-            }
-        }
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int number = scanner.nextInt();
 
+        boolean result = number % 2 == 0;
+        if (result) {
+            System.out.println("четное");
+        } else {
+            System.out.println("Нечетное");
+
+        }
+
+    }
+}
