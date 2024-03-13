63acf765e4cd09c0ae77156c6af9d2eadcdc7235
PolinaQAe <138627297+PolinaQAe@users.noreply.github.com>
2024-03-13 14:53:21 +0300

    Update CyclesTwo.java

--- CyclesTwo.java
+++ CyclesTwo.java
@@ -1,12 +1,14 @@
-public class CyclesTwo {
-public static void main (String[] args){
-int i = 1;
-int number = 9;
-int sum = 0;
-hile (i <= number) {
-sum += i;
-i++; }
-{
- System.out.println("Сумма чисел от 1 до " + number + " равна " + sum); }
-  }
+public class Cy {
+    public static void main(String[] args) {
+        int i = 1;
+        int number = 9;
+        int sum = 0;
+        while (i <= number) {
+            sum += i;
+            i++;
+        }
+        {
+            System.out.println("Сумма чисел от 1 до " + number + " равна " + sum);
+        }
+    }
 }
